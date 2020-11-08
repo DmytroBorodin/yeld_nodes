c4e5a2e81be0cd185d3a22504aaa4da2f28bd70d
Dmytro Borodin <dimabor96@gmail.com>
2020-11-08 06:26:37 +0200

    08.11.2020

--- js/main.js
+++ js/main.js
@@ -19,3 +19,28 @@ window.addEventListener('scroll', function() {
 		navBar.classList.remove('navBg');
 	}
 });
+
+
+let popup = document.querySelector('.popup');
+let popupWrapper = popup.querySelector('.popup-wrapper');
+let body = document.querySelector('body');
+let closeBtn = document.querySelector('.close-btn');
+let videos = [...document.querySelectorAll('.page-video-wrapper')];
+
+videos.forEach(video => {
+	video.addEventListener('click', () => {
+		let parent = video.parentNode;
+		let iframe = parent.querySelector('iframe');
+		let iframeClone = iframe.cloneNode(true);
+		popup.classList.add('active');
+		body.classList.add('locked');
+		popupWrapper.appendChild(iframeClone);
+	})
+});
+
+closeBtn.addEventListener('click', () => {
+	let iframe = popupWrapper.querySelector('iframe');
+	popup.classList.remove('active');
+	body.classList.remove('locked');
+	popupWrapper.removeChild(iframe);
+})
